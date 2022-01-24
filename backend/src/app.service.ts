10b85b681eb904c1b72fa712e2fc0f9f50e06cbf
diegomagalhaes-dev <digengup@gmail.com>
2022-01-23 22:07:07 -0300

    :zap: create a simple user service

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -5,4 +5,12 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getUser(): Record<string, unknown> {
+    return {
+      name: 'Diêgo',
+      idade: 21,
+      profissao: 'Software Engeenir',
+    };
+  }
 }
