8fcf804b3f401707e97a82ebd7e37e915464ec5c
diegomagalhaes-dev <digengup@gmail.com>
2022-01-25 22:43:53 -0300

    :hammer: refactoring to return token object

--- backend/src/auth/auth/auth.controller.ts
+++ backend/src/auth/auth/auth.controller.ts
@@ -6,7 +6,7 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
-  login(@Body() body: any): void {
-    this.authService.login(body.username, body.password);
+  login(@Body() body: any) {
+    return { token: this.authService.login(body.username, body.password) };
   }
 }
