29e10fa54673abe866496f95e0c46afdff286498
diegomagalhaes-dev <digengup@gmail.com>
2022-01-30 22:18:25 -0300

    :sparkles: create jwt auth

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: process.env.TYPEORM_CONNECTION as any,
+      host: process.env.TYPEORM_HOST,
+      port: parseInt(process.env.TYPEORM_PORT),
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      entities: [__dirname + '/models/*.entity{.js,.ts}'],
+      migrations: [__dirname + '/database'],
+    }),
+    AuthModule,
+    UsersModule,
+  ],
 })
 export class AppModule {}
